9b41898e1176e8b69ef4e1d38984e6d359100e99
Marco;Piredda;5263590 <101405992+Marcopire@users.noreply.github.com>
2022-03-30 13:49:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+   int a,b,x,y;
+   if (x<0 && y>0) {
+      cout << a*x*x-b*y <<endl;
+   } else if (x>=0 && y<=0) {
+      cout << a*x*x-b*y << endl;
+   } else {
+      cout << a*x+b*y*y << endl;
+   }
    return 0;
 }
